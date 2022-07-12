fa2160d163984b3110c8054133c804b2d9dc007a
Zinzere <58589995+Zinzere@users.noreply.github.com>
2022-07-12 09:07:11 +0530

    Update for CurdForm

--- Inventory/Dev/Flutter/inventory/lib/ui/SearchField.dart
+++ Inventory/Dev/Flutter/inventory/lib/ui/SearchField.dart
@@ -1,5 +1,6 @@
 /*
 SearchField(
+  controller: TextEdittingController(),
   showKeyList: ["NAME","-","TYPE"],
   futureOrList: (val) async {
     var data = await Calls.search({"name":val});
@@ -21,7 +22,15 @@ import 'package:flutter/material.dart';
 typedef FutureOr<dynamic> futureListCallBack(String pattern);
 
 class SearchField extends StatefulWidget {
-  SearchField({@required this.onSelected,this.controller, this.showKeyList, this.futureOrList, this.width = 400, this.label = "Search Here"});
+  SearchField({@required
+      this.onSelected,
+      this.controller,
+      this.showKeyList,
+      this.futureOrList,
+      this.width = 400,
+      this.decoration,
+      this.textAlign=TextAlign.start,
+      this.label = ""});
 
   final Function(dynamic) onSelected; //If selected returns
   final futureListCallBack futureOrList; //Future or List
@@ -29,6 +38,9 @@ class SearchField extends StatefulWidget {
   final String label; //Label of Search text Field
   final List<String> showKeyList; //List of Key's value will be shown in Search List
   final TextEditingController controller;
+  final InputDecoration decoration;
+  final TextAlign textAlign;
+
   @override
   State<SearchField> createState() => _SearchFieldState();
 }
@@ -53,8 +65,9 @@ class _SearchFieldState extends State<SearchField> {
     return IntrinsicWidth(
       stepWidth: widget.width,
       child: TextField(
+        textAlign: widget.textAlign,
         controller: widget.controller,
-        decoration: InputDecoration(
+        decoration: widget.decoration ?? InputDecoration(
             contentPadding: EdgeInsets.only(left: 2.0, right: 2),
             labelText: widget.label,
             labelStyle: TextStyle(
@@ -67,6 +80,7 @@ class _SearchFieldState extends State<SearchField> {
           Navigator.push(context,
               PopupWidget(
                   controllers: widget.controller,
+                  textAlign: widget.textAlign,
                   buttonRect: itemRect,
                   futureOrList: widget.futureOrList,
                   showKeyList: widget.showKeyList,
@@ -77,9 +91,13 @@ class _SearchFieldState extends State<SearchField> {
               ));
         },
         onChanged: (val) {
+          if (!positionCheck) {
+            getPosition();
+          }
           Navigator.push(context,
               PopupWidget(
                   controllers: widget.controller,
+                  textAlign: widget.textAlign,
                   buttonRect: itemRect,
                   futureOrList: widget.futureOrList,
                   showKeyList: widget.showKeyList,
@@ -95,9 +113,19 @@ class _SearchFieldState extends State<SearchField> {
 }
 
 class PopupWidget extends PopupRoute {
-  PopupWidget({this.buttonRect,this.controllers, this.screenSize, this.futureOrList, this.onSelected, this.width, this.label, this.showKeyList});
+  PopupWidget({
+    this.buttonRect,
+    this.controllers,
+    this.textAlign,
+    this.screenSize,
+    this.futureOrList,
+    this.onSelected,
+    this.width,
+    this.label,
+    this.showKeyList});
 
   final TextEditingController controllers;
+  final TextAlign textAlign;
   final Rect buttonRect;
   final futureListCallBack futureOrList;
   final Function(dynamic) onSelected;
@@ -127,6 +155,7 @@ class PopupWidget extends PopupRoute {
   @override
   Widget buildPage(BuildContext context, Animation<double> animation,Animation<double> secondaryAnimation) {
     bool upOrDown = _upDownCheck(buttonRect.top,screenSize.height);
+    bool heightCheck = false;
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return StatefulBuilder(
@@ -136,7 +165,8 @@ class PopupWidget extends PopupRoute {
                   if (this.showKeyList != null) {
                     for (var i = 0; i < this.showKeyList.length; i++) {
                       String displayKey = this.showKeyList[i];
-                      displayText = displayText + (kv[displayKey] ?? displayKey);
+                      var val = kv[displayKey] ?? displayKey;
+                      displayText = displayText + val.toString();
                     }
                   } else {
                     displayText = displayText + kv[kv.keys[0]];
@@ -155,24 +185,35 @@ class PopupWidget extends PopupRoute {
                     child: Align(child: Text(displayText, textAlign: TextAlign.start,), alignment: Alignment.centerLeft,),
                   );
                 }
-                Widget txtField(){
+                Widget txtField(width){
                   return Container(
+                    height: 35,
                     color:Theme.of(context).canvasColor,
-                    child:TextField(
-                        autofocus: true,
-                        controller:controllers,
-                        decoration: InputDecoration(
-                          contentPadding: EdgeInsets.only(left: 2.0, right: 2),
-                        ),
-                        onChanged:(val) async {
-                          if(val.length>0){
-                            searchResult = await this.futureOrList(val);
-                            if(searchResult.length>7) {
-                             searchResult = searchResult.sublist(0,7);
+                    child:IntrinsicWidth(
+                      stepWidth: width,
+                      child: TextField(
+                          textAlign: textAlign,
+                          autofocus: true,
+                          controller:controllers,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(left: 2.0, right: 2,bottom: 3),
+                          ),
+                          onChanged:(val) async {
+                            if(val.length>0){
+                              heightCheck=true;
+                              searchResult = await this.futureOrList(val);
+                              if(searchResult.length>7) {
+                               searchResult = searchResult.sublist(0,7);
+                              }
+                            } else {
+                              heightCheck=false;
+                              searchResult = [];
+                              this.onSelected({});
+                              _dismiss();
                             }
                             setState(() => {});
                           }
-                        }
+                      ),
                     ),
                   );
                 }
@@ -180,7 +221,8 @@ class PopupWidget extends PopupRoute {
                   delegate: SingleChildDelegateWidget(
                       buttonRect: buttonRect,
                       screenSize: screenSize,
-                      upOrDown:upOrDown
+                      upOrDown:upOrDown,
+                      heightCheck:heightCheck,
                   ),
                   child: Material(
                     color: Colors.transparent,
@@ -191,7 +233,6 @@ class PopupWidget extends PopupRoute {
                        reverseCurve: const Interval(0.75,1.0),
                       ),
                       child: Container(
-                        width: width,
                         child: upOrDown ? Column( //True ? Up : Down
                           children:[
                             SizedBox(
@@ -204,11 +245,11 @@ class PopupWidget extends PopupRoute {
                                 ],
                               ),
                             ),
-                            txtField(),
+                            txtField(width),
                           ],
                         ) : Column(
                           children:[
-                            txtField(),
+                            txtField(width),
                             ListView(
                               padding: EdgeInsets.only(top: 2),
                               shrinkWrap: true,
@@ -230,11 +271,12 @@ class PopupWidget extends PopupRoute {
 }
 
 class SingleChildDelegateWidget extends SingleChildLayoutDelegate {
-  SingleChildDelegateWidget({this.buttonRect, this.screenSize,this.upOrDown});
+  SingleChildDelegateWidget({this.buttonRect, this.screenSize,this.upOrDown,this.heightCheck});
 
   final Rect buttonRect;
   final Size screenSize;
   bool upOrDown;
+  bool heightCheck;
 
   @override
   BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
@@ -242,12 +284,12 @@ class SingleChildDelegateWidget extends SingleChildLayoutDelegate {
     if(upOrDown){ //True => Up
       height = 260;
     } else { //False => Down
-      height = screenSize.height + 100;//Good to go
+      height = screenSize.height + 100;//Good to go screenSize.height + 100
     }
     return BoxConstraints(
       minWidth: buttonRect.width,
       maxWidth: buttonRect.width,
-      maxHeight: height,
+      maxHeight: heightCheck ? height : buttonRect.height,
     );
   }
 
@@ -256,7 +298,7 @@ class SingleChildDelegateWidget extends SingleChildLayoutDelegate {
     if(upOrDown){ //True => Up
       return Offset(buttonRect.left,buttonRect.top * 0.61);
     } else { //False => Down
-      return Offset(buttonRect.left,buttonRect.top + 1); //Good to go
+      return Offset(buttonRect.left,buttonRect.top + 3); //Good to go | buttonRect.left,buttonRect.top + 1
     }
   }
 
